7dd5760ab144f159de8adc60922af8b6db4f8115
Peter <peter130840@hotmail.com>
2019-10-19 00:08:49 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log('starting');
+
+setTimeout(()=>{
+    console.log('2 Second Timer');    
+}, 2000);
+
+setTimeout(()=>{
+    console.log('0 Second Timer')
+}, 0)
+
+console.log('stopping');

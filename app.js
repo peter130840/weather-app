51a6b185d8dc71f26aaf7e646ee8353e958be246
Peter <peter130840@hotmail.com>
2019-10-20 02:59:57 +0800

    impliment error handling

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 require('dotenv').config();
 const request = require('request');
-const url = `https://api.darksky.net/forecast/${process.env.SECRETKEY}/25.08,121.44?lang=zh-tw&units=si`;
+//to fetch the latitude and longitude from mapbox
+// let longtitute,latitute;
+// const urlMapbox = `https://api.mapbox.com/geocoding/v5/mapbox.places/Taipei%20City.json?access_token=${process.env.MAPBOXSECRETKEY}&limit=1`
+// request({url: urlMapbox, json: true},(error, response) =>{
+//     latitute = response.body.features[0].center[1];
+//     longtitute = response.body.features[0].center[0];
+//     console.log('latitute: ' + latitute + ' longtitute: ' + longtitute);
 
-request({ url: url, json: true}, (error, response) => {
-    console.log('It is curently ' + response.body.currently.temperature + ' degrees out.')
-    console.log('there is a ' + response.body.currently.precipProbability + '% chance of rain.')
-})
+// })
+
+
+const urlDarksky = `https://api.darksky.net/forecast/${process.env.DARKSKYSECRETKEY}/25.08,121.44?lang=zh-tw&units=si`;
+
+
+request({ url: urlDarksky, json: true}, (error, response) => {
+    if(error){
+        console.log('Unable to connect to weather service!');
+    } else if(response.body.error){
+        console.log('Unable to find location')
+    }
+    else{
+        console.log('It is curently ' + response.body.currently.temperature + ' degrees out.')
+        console.log('there is a ' + response.body.currently.precipProbability + '% chance of rain.')
+    }
+
+});

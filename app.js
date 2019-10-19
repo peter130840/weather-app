549856bd59b418811988ad4edc70bc3a849f5d29
Peter <peter130840@hotmail.com>
2019-10-19 17:46:04 +0800

    get weather of Taipei from darkday

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 const request = require('request');
-const url = `https://api.darksky.net/forecast/${process.env.SECRETKEY}/37.8267,-122.4233`;
+const url = `https://api.darksky.net/forecast/${process.env.SECRETKEY}/25.08,121.44?lang=zh-tw&units=si`;
 
 request({ url: url, json: true}, (error, response) => {
-    console.log(response.body.currently);
+    console.log('It is curently ' + response.body.currently.temperature + ' degrees out.')
+    console.log('there is a ' + response.body.currently.precipProbability + '% chance of rain.')
 })

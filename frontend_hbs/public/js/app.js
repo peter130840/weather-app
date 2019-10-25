487f60fecccfeb53544a50e6f0b56c69750f248e
Peter <peter130840@hotmail.com>
2019-10-25 19:45:59 +0800

    Change listening port to unassigned,frontend fetch url to root address

--- frontend_hbs/public/js/app.js
+++ frontend_hbs/public/js/app.js
@@ -16,7 +16,7 @@ weatherForm.addEventListener('submit', (e) => {
 
 
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             console.log(data.error)
             if (data.error) {
